cd5536da8fb61b9fab033dfa3303234409d73dcc
Amin dr <dr.amin.std@gmail.com>
2023-03-20 19:47:55 -0700

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - Alphabeth Soup
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if (c != 'q' && c != 'z')
+				putchar(c);
+			c++;
+	}
+	putchar('\n');
+	return (0);
+}
