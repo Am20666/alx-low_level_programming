ea1b44bd285ed4875012686f6c4bcb77fcef632e
Amin dr <dr.amin.std@gmail.com>
2023-03-20 18:46:28 -0700

    alphabet soup

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet.
+ *
+ * Return:cAlways 0 (Success)
+ */
+int main(void)
+{
+	char alph[26] = "abcdefgklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
