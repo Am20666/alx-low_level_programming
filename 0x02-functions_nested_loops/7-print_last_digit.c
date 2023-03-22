07b37b8933474016e5cce529206e09fbce1056aa
Amin dr <dr.amin.std@gmail.com>
2023-03-22 12:33:28 -0700

    lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,10 +2,11 @@
 /**
  * print_last_digit - print last digit of a number.
  *
- * @n: takes nymber input
+ * @n: takes number input
  *
  * Return: lastDigit
  */
+
 int print_last_digit(int n)
 {
 	int lastDigit;
@@ -13,7 +14,7 @@ int print_last_digit(int n)
 	if (n < 0)
 		lastDigit = -1 * (n % 10);
 	else
-		lastDigit = n & 10;
+		lastDigit = n % 10;
 
 	_putchar(lastDigit + '0');
 	return (lastDigit);
