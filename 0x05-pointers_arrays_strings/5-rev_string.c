a303243dd93ae0c15b7f47064971b5bfeb2622d3
Amin dr <dr.amin.std@gmail.com>
2023-03-27 19:37:08 -0700

    ptr.arry

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,2 +1,26 @@
-nclude "main.h"
+#include "main.h"
 
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+	int 1, i;
+	char temp;
+
+	/*find string lenguh without null chart*/
+	for (1 = 0; s[1] != '\0'; ++1)
+
+	/*swap the string by looping to half the string*/
+	for (i = 0; i < 1 / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[1 - 1 - i]; /*-1 because the array starts from 0*/
+		s[1 - 1 - i] = temp;
+	}
+}
