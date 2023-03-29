5a05ce6b7e25da651cf248472088c44710c10d1e
Amin dr <dr.amin.std@gmail.com>
2023-03-29 09:52:51 -0700

    ptr.arry

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,16 @@
 char *_strcat(char *dest, char *src)
 {
 	int c, c2;
+	int C = 0;
 
-	C = 0;
 	/*find the size of dest array*/
 	while (dest[c])
 		c++;
+
 	/*iterate through each src array value without the null byte*/
 	for (c2 = 0; src[c2]; c2++)
 		/*append src[c2] to dest[c]) while overwritting the null byte in dest*/
 		dest[C++] = src[c2];
+
 	return (dest);
 }
