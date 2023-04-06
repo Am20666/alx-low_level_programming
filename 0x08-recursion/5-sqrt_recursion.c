5ab4f3fe7e2bfb7f6bc72c2f7b3398f992f4c34b
Amin dr <dr.amin.std@gmail.com>
2023-04-05 19:50:54 -0700

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -22,7 +22,7 @@ int helperFunction(int num, int pSqrt)
 }
 
 /**
- * sqrt_recursion - returns the natural square root of a number.
+ * _sqrt_recursion - returns the natural square root of a number.
  * @n: number to find sqrt of.
  *
  * Return: squareroot of n.
