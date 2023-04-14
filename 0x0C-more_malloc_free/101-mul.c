07bd2ad1733f2335dd774a71fcd2dd8cb9b90e77
Amin dr <dr.amin.std@gmail.com>
2023-04-13 19:42:17 -0700

    more malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,7 +4,8 @@
  * _puts - prints a string, followed by a new line,
  * @str: pointer to the string to print
  * Return: void
- */
+*/
+
 
 void _puts(char *str)
 {
@@ -27,7 +28,7 @@ int _atoi(const char *s)
 {
 	int sign = 1;
 	unsigned long int resp = 0, firstNum, i;
-	
+
 	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
 	{
 		if (s[firstNum] == '-')
@@ -55,7 +56,6 @@ void print_int(unsigned long int n)
 
 	for (i = 0; n / divisor > 9; i++, divisor *= 10)
 	;
-	
 	for (; divisor >= 1; n %= divisor, divisor /= 10)
 	{
 		resp = n / divisor;
@@ -66,3 +66,21 @@ void print_int(unsigned long int n)
 /**
  * main - print the result of the multiplication, followed by a new line
  * @argc: int
+ * @argv: list
+ * *Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void) argc;
+
+	if (argc != 3)
+	{
+		_puts("Error");
+		exit(98);
+	}
+	print_int(_atoi(argv[1]) * _atoi(argv[2]));
+	_putchar('\n');
+
+	return (0);
+}
