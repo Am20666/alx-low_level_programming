e014e0f90ca26959ae3df88565c22c2a42a2bb42
Amin dr <dr.amin.std@gmail.com>
2023-04-13 17:04:35 -0700

    more malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,7 +21,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NUll);
 	}
 	if (ptr == NULL)
-	}
+	{
 		p = malloc(new_size);
 		if (p == NULL)
 			return (NULL);
