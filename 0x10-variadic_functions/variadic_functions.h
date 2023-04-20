2599fac2d14cf568d942b598c898a392658b0a12
Amin dr <dr.amin.std@gmail.com>
2023-04-20 16:37:08 -0700

    variadic_functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -17,6 +17,6 @@ typedef struct token
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, --);
+void print_all(const char * const format, ...);
 
 #endif
