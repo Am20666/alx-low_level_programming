79e05ef2fbe87186822c43fb0167ca6fe1f66d23
Amin dr <dr.amin.std@gmail.com>
2023-07-11 09:39:01 -0700

    task 6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * get_endianness - function
- * Return: Always 0.
+ * get_endianness - checks endianness.
+ * Return: int.
  */
-
 int get_endianness(void)
 {
-	int i = 1;
+	unsigned int i = 1;
+	char *c = (char *) &i;
 
-	if (*(char *)&i)
+	if (*c)
 		return (1);
-	return (0);
+	else
+		return (0);
 }
 
