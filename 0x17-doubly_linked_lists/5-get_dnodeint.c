acc8def1b20b1338b8d5aa1a21480bd59832699d
Amin dr <dr.amin.std@gmail.com>
2023-08-18 23:48:16 +0100

    doubly_linked_lists

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,27 +1,20 @@
 #include "lists.h"
-
 /**
- * get_dnodeint_at_index - eturns the nth node of a dlistint_t linked list.
- * @head: double pointer to the head of the list
- * @index: index of the node to return
- *
- * Return: if the node does not exist, return NULL
- *          where index is the index of the node, starting from 0
- */
-
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+ * get_dnodeint_at_index - returns node at given index
+* @head: pointer to current head node
+* @index: index of node to return
+* Return: address of node at index
+*/
+dlistint_t *get_dnodeint_at_index (dlistint_t *head, unsigned int index)
 {
-	dlistint_t *current;
-	unsigned int i;
-
-	current = head;
-	i = 0;
-	while (current != NULL)
-	{
-		if (i == index)
-			return (current);
-		current = current->next;
-		i++;
-	}
-	return (NULL);
+    unsigned int i = 0;
+        
+    while (head)
+    {
+        if (i == index)
+            return (head);
+        head = head->next;
+        i++;
+    }
+    return (NULL);
 }
